af72382f38552dd3568def07c84512921369c8c6
Walter Nick Suel Arroyo <nicodehero21@outlook.com>
2023-03-05 13:45:20 -0500

    Proporcionando al proyecto los archivos jar de conexion

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Conexion;
 
-/**
- *
- * @author nick_
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
 public class Conexion {
-    
+
+    private String db = "punto_de_ventas";
+    private String user = "root";
+    private String password = "1234";
+    private String urlMysql = "jdbc:mysql://localhost/" + db + "?SslMode=none";
+    private Connection conn = null;
+
+    public Conexion() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(this.urlMysql,this.user,this.password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: "+e);
+        }
+    }
 }
